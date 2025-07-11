97c1443be40bc7265ebf7e66678af6d021dce993
khushi <khushik10201@gmail.com>
2025-07-11 16:34:36 +0530

    added

---
+++ tf-gpu-env/bin/activate.csh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:68b50a35e0ee7944fa351dc86d56d0eb22592042547512fd301d440c10e791ee
+size 944
---
+++ tf-gpu-env/bin/activate.fish
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:208038144345062935a1201edd7d8103a9f3c9bd767f963c4269724063e8d680
+size 2220
